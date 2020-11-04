2d33c34c54bd7eb6ffa9b532f8fd306e172a7096
Javier <azpe@uma.es>
2020-11-04 17:41:21 +0100

    agregando prueba

---
+++ Analisis lexico/prueba/TablaSimbolos.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class TablaSimbolos {
+	protected static HashMap<String,Integer> hm = new HashMap<String,Integer>();
+	
+	public static void put(String variable, int valor) {
+		hm.put("$"+variable, valor);
+	}
+	
+	public static int get(String variable) {
+		int valor = hm.get(variable);
+		if(hm.containsKey(variable){
+			return valor;
+		}else{
+			return 0;
+		}
+	}
+
+	public static void dump() {
+		System.out.println("-------------");
+		for (String item : hm.keySet()) {
+		    System.out.println(item+"="+hm.get(item));
+		}	
+	}
+
+}
---
+++ Analisis lexico/prueba/wc.java
@@ -0,0 +1,25 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class wc {
+    public static int nl=0;
+    public static int np=0;
+    public static int nc=0;
+    
+    public static void main(String arg[]) {
+    
+        if (arg.length>0) {
+            try {
+
+                Yylex lex = new Yylex(new FileReader(arg[0]));
+                lex.yylex();
+                
+                System.out.println(nl+"\t"+np+"\t"+nc+"\t"+arg[0]);
+            
+                
+	        } catch (IOException e) {
+	    }
+        }
+    }
+
+}
